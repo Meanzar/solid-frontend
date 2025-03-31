db7c50f168c145c1cebf89d03016a587087c93de
meanzar <116259100+Meanzar@users.noreply.github.com>
2025-03-31 15:05:02 +0200

    register & login && news api

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { postData } from '@/lib/api'
+import React, { useState } from 'react'
+
+export default function RegisterPage() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const base_url = "/api/auth/login"
+    const handleButton = async () => {
+        postData(base_url, {email, password})
+
+    }  
+    function handleChange(set: any, e: any) {
+           set(e.target.value)
+    }
+  return (
+    <div>
+        <div>
+            <label>Email</label>
+            <Input type="email" value={email} onChange={(e) => handleChange(setEmail, e)} />
+            <label>Password</label>
+            <Input type="password" value={password} onChange={(e) => handleChange(setPassword, e)}/>
+            <Button onClick={handleButton}>Click mEI jdioej </Button>
+        </div>
+    </div>
+  )
+}
