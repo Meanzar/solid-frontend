df4b7827965f5a941ef8d505c6a063b91862f3ce
meanzar <116259100+Meanzar@users.noreply.github.com>
2025-03-31 16:01:03 +0200

    bug login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -65,7 +65,7 @@ export default function LoginPage() {
                 </div>
 
                 <Button 
-                    onClick={handleButton} 
+                    onClick={handleSubmit} 
                     className="w-full py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 >
                     Login
